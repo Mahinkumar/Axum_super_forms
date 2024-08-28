e856063b5755d1219462687548753498fde28f68
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-08-28 22:41:11 +0530

    Embed SPA into executable

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use axum::Router;
 use std::net::SocketAddr;
-use tower_cookies::CookieManagerLayer;
-use tower_http::services::{ServeDir, ServeFile};
+
+pub mod router;
+use router::using_serve_dir;
 
 //use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 //use tower_http::trace::TraceLayer;
 
 const ADDR: [u8; 4] = [127, 0, 0, 1];
 const PORT: u16 = 8000;
-const FRONTEND_PATH: &str = "./client/dist";
 
 #[tokio::main]
 
@@ -26,23 +26,14 @@ async fn main() {
     tokio::join!(serve(using_serve_dir(), PORT),);
 }
 
-fn using_serve_dir() -> Router {
-    Router::new()
-        .nest_service(
-            "/",
-            ServeDir::new(format!("{FRONTEND_PATH}/"))
-                .not_found_service(ServeFile::new(format!("{FRONTEND_PATH}/index.html"))),
-        )
-        .nest_service("/assets",ServeDir::new(format!("{FRONTEND_PATH}/assets")))
-        .layer(CookieManagerLayer::new())
-}
+
 
 async fn serve(app: Router, port: u16) {
     println!("Serving on address: http://127.0.0.1:{PORT}");
     let addr = SocketAddr::from((ADDR, port));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app
-        //.layer(TraceLayer::new_for_http())
+        
     )
         .await
         .unwrap();
---
+++ src/router.rs
@@ -0,0 +1,56 @@
+use axum::Router;
+use tower_cookies::CookieManagerLayer;
+use axum::{
+    http::{header, StatusCode, Uri},
+    response::{Html, IntoResponse, Response},
+};
+use rust_embed::Embed;
+
+static INDEX_HTML: &str = "index.html";
+
+#[derive(Embed)]
+#[folder = "./client/dist/"]
+struct Assets;
+
+//use tower_http::trace::TraceLayer;
+
+pub fn using_serve_dir() -> Router {
+    Router::new()
+        .fallback(static_handler)
+        .layer(CookieManagerLayer::new())
+    //  .layer(TraceLayer::new_for_http()) // For Debug only
+}
+
+async fn static_handler(uri: Uri) -> impl IntoResponse {
+    let path = uri.path().trim_start_matches('/');
+
+    if path.is_empty() || path == INDEX_HTML {
+        return index_html().await;
+    }
+
+    match Assets::get(path) {
+        Some(content) => {
+            let mime = mime_guess::from_path(path).first_or_octet_stream();
+
+            ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+        }
+        None => {
+            if path.contains('.') {
+                return not_found().await;
+            }
+
+            index_html().await
+        }
+    }
+}
+
+async fn index_html() -> Response {
+    match Assets::get(INDEX_HTML) {
+        Some(content) => Html(content.data).into_response(),
+        None => not_found().await,
+    }
+}
+
+async fn not_found() -> Response {
+    (StatusCode::NOT_FOUND, "404").into_response()
+}
