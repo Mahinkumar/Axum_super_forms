2a709649b56d61e3a58bf6f39610ae102b6a25cb
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-08-29 21:48:09 +0530

    Setup JWT Numeric Dates

--- src/auth.rs
+++ src/auth.rs
@@ -1,31 +1,86 @@
 use serde::{Deserialize, Serialize};
 
-use axum::{
-    body::Body,
-    extract::Request,
-    http,
-    http::Response,
-    middleware::Next,
-};
+use axum::{body::Body, extract::Request, http, http::Response, middleware::Next};
+
+use time::{Duration, OffsetDateTime};
 
 use jsonwebtoken::errors::ErrorKind;
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Claims {
     sub: String,
     user: String,
-    exp: u64,
+    #[serde(with = "jwt_numeric_date")]
+    iat: OffsetDateTime,
+    #[serde(with = "jwt_numeric_date")]
+    exp: OffsetDateTime,
 }
 
-const KEY: &[u8] = b"TheUltiateKey";
+mod jwt_numeric_date {
+    //! Custom serialization of OffsetDateTime to conform with the JWT spec (RFC 7519 section 2, "Numeric Date")
+    use serde::{self, Deserialize, Deserializer, Serializer};
+    use time::OffsetDateTime;
 
-pub fn create_token(email: &str,username: &str)-> String {
-    let my_claims =
-        Claims { sub: email.to_owned(), user: username.to_owned(), exp: 10000000000 };
-    
-    let header =
-        Header { kid: Some("signing_key".to_owned()), alg: Algorithm::HS512, ..Default::default() };
+    /// Serializes an OffsetDateTime to a Unix timestamp (milliseconds since 1970/1/1T00:00:00T)
+    pub fn serialize<S>(date: &OffsetDateTime, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let timestamp = date.unix_timestamp();
+        serializer.serialize_i64(timestamp)
+    }
+
+    /// Attempts to deserialize an i64 and use as a Unix timestamp
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<OffsetDateTime, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        OffsetDateTime::from_unix_timestamp(i64::deserialize(deserializer)?)
+            .map_err(|_| serde::de::Error::custom("invalid Unix timestamp value"))
+    }
+}
+
+impl Claims {
+    /// If a token should always be equal to its representation after serializing and deserializing
+    /// again, this function must be used for construction. `OffsetDateTime` contains a microsecond
+    /// field but JWT timestamps are defined as UNIX timestamps (seconds). This function normalizes
+    /// the timestamps.
+    pub fn new(sub: String, user: String, iat: OffsetDateTime, exp: OffsetDateTime) -> Self {
+        // normalize the timestamps by stripping of microseconds
+        let iat = iat
+            .date()
+            .with_hms_milli(iat.hour(), iat.minute(), iat.second(), 0)
+            .unwrap()
+            .assume_utc();
+        let exp = exp
+            .date()
+            .with_hms_milli(exp.hour(), exp.minute(), exp.second(), 0)
+            .unwrap()
+            .assume_utc();
+
+        Self {
+            sub,
+            user,
+            iat,
+            exp,
+        }
+    }
+}
+
+const KEY: &[u8] = b"TheUltimateKey";
+
+pub fn create_token(email: &str, username: &str) -> String {
+    let iat = OffsetDateTime::now_utc();
+    let exp = iat + Duration::days(1);
+
+    let my_claims = Claims::new(email.to_owned(), username.to_owned(), iat, exp);
+
+    let header = Header {
+        kid: Some("signing_key".to_owned()),
+        alg: Algorithm::HS512,
+        ..Default::default()
+    };
 
     let token = match encode(&header, &my_claims, &EncodingKey::from_secret(KEY)) {
         Ok(t) => t,
@@ -35,7 +90,7 @@ pub fn create_token(email: &str,username: &str)-> String {
     token
 }
 
-pub fn decode_token(token: String)->Header{
+pub fn decode_token(token: String) -> Header {
     let token = match decode::<Claims>(
         &token,
         &DecodingKey::from_secret(KEY),
@@ -47,12 +102,12 @@ pub fn decode_token(token: String)->Header{
             _ => panic!(),
         },
     };
-    token.header
     //println!("{:?}",token.header);
     //println!("{:?}",token.claims);
+    token.header
 }
 
-pub async fn authorization_middleware(mut req: Request, next: Next)->Response<Body>{
+pub async fn authorization_middleware(mut req: Request, next: Next) -> Response<Body> {
     let _auth_header = req.headers_mut().get(http::header::AUTHORIZATION);
     //println!("{:?}",auth_header);
     //We Do the authentication here or redirect to login page
