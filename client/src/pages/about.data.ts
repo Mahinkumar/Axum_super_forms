d82fe46d51ce6df78dd440c36c311acf2f217738
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-08-29 12:10:04 +0530

    Setup Simple Keycode Login Page

--- client/src/pages/about.data.ts
+++ client/src/pages/about.data.ts
@@ -2,6 +2,8 @@ import type { RouteDataFunc } from '@solidjs/router';
 import { getCookie, setCookie } from 'typescript-cookie'
 import { createResource, Resource } from 'solid-js';
 
+let x: number;
+
 function wait<T>(ms: number, data: T): Promise<T> {
   return new Promise((resolve) => setTimeout(resolve, ms, data));
 }
@@ -12,13 +14,14 @@ function random(min: number, max: number): number {
 
 function fetchName(): Promise<string> {
   setCookie('Cookie_from', 'browser');
-  return wait(random(500, 1000), 'Solid');
+  x = random(500, 1000);
+  return wait(random(500, 1000), 'Solid in '+ x + ' ms' );
 }
 
 const AboutData: RouteDataFunc<never, Resource<string>> = () => {
   const [data] = createResource(fetchName);
-
   return data;
+  
 };
 
 export default AboutData;
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,30 +1,71 @@
-import { createSignal } from 'solid-js';
+import { createMemo, createSignal } from "solid-js";
 
 export default function Home() {
-  const [count, setCount] = createSignal(0);
+  const [is_auth, setis_auth] = createSignal(false);
+  const handleButtonClick = () => setis_auth(!is_auth());
 
   return (
-    <section class="bg-gray-100 text-gray-700 p-8">
-      <h1 class="text-2xl font-bold">Axum Login Page</h1>
-      <p class="mt-4">This is a test Login page.</p>
-
-      <div class="flex items-center space-x-2">
-        <button
-          class="border rounded-lg px-2 border-gray-900"
-          onClick={() => setCount(count() - 1)}
-        >
-          -
-        </button>
-
-        <output class="p-10px">Count: {count()}</output>
-
-        <button
-          class="border rounded-lg px-2 border-gray-900"
-          onClick={() => setCount(count() + 1)}
-        >
-          +
-        </button>
+    <>
+      <div class="bg-gray-100 p-4 flex flex-col justify-center items-center w-screen h-screen">
+        <div class="w-96 h-80 bg-white rounded-lg shadow p-6 flex flex-col justify-around">
+          <h1 class="text-3xl text-center">Axum Login</h1>
+          <button onClick={handleButtonClick}>
+            {is_auth() ? "Via Auth" : "Via KeyCode"}
+          </button>
+          {is_auth() ? <Auth_form /> : <Key_form />}
+        </div>
       </div>
-    </section>
+    </>
   );
+}
+
+function Auth_form() {
+  return (
+    <>
+      <form class="space-y-4 flex flex-col justify-around items-center">
+        <input
+          type="email"
+          placeholder="Email Address"
+          class="block w-[80%] px-4 py-2 text-gray-700 border 
+rounded-lg appearance-none focus:outline-none focus:border-blue-300 
+bg-gray-100"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          class="block w-[80%] px-4 py-2 text-gray-700 border 
+rounded-lg appearance-none focus:outline-none focus:border-blue-300 
+bg-gray-100"
+        />
+        <button
+          class="w-48 h-12 text-white transition duration-200 ease-out bg-green-500 hover:bg-green-600 rounded-lg self-center"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </>
+  );
+}
+
+function Key_form() {
+  return (
+    <>
+      <form class="space-y-4 flex flex-col justify-around items-center">
+        <input
+          type="password"
+          placeholder="Key"
+          class="block w-[80%] px-4 py-2 text-gray-700 border 
+rounded-lg appearance-none focus:outline-none focus:border-blue-300 
+bg-gray-100"
+        />
+        <button
+          class="w-48 h-12 text-white transition duration-200 ease-out bg-green-500 hover:bg-green-600 rounded-lg self-center"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </>
+  )
 }
