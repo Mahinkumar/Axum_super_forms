8c603f7be03916b5869147ce6d532596b569680a
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-09-03 09:50:54 +0530

    Setup Admin auth

--- src/client.rs
+++ src/client.rs
@@ -25,11 +25,32 @@ pub struct FormsTemplate<'a> {
     id: &'a str,
 }
 
+#[derive(Template)]
+#[template(path = "admin.html")]
+pub struct AdminTemplate<'a> {
+    name: &'a str,
+}
+
+#[derive(Template)]
+#[template(path = "adminForm.html")]
+pub struct AdminFormTemplate<'a> {
+    id: &'a str,
+}
+
+
+#[derive(Template)]
+#[template(path = "adminstat.html")]
+pub struct AdminStatTemplate<'a> {
+    id: &'a str,
+}
+
+
 pub fn service_router() -> Router {
     Router::new()
         .route("/", get(home))
         .route("/login", get(login))
         .route("/forms", get(forms))
+        .route("/admin", get(admin))
         .route_service(
             "/output.css",
             ServeFile::new("./templates/assets/output.css"),
@@ -39,7 +60,7 @@ pub fn service_router() -> Router {
 }
 
 pub async fn home(cookies: Cookies) -> Response<Body> {
-    if verify_cookie(&cookies).await {
+    if !verify_cookie(&cookies).await.0 {
         return to_login().await;
     }
     let home = HelloTemplate { name: "world" }; // instantiate your struct
@@ -52,15 +73,26 @@ pub async fn login() -> Response<Body> {
 }
 
 pub async fn forms(cookies: Cookies) -> Response<Body> {
-    if verify_cookie(&cookies).await {
+    if !verify_cookie(&cookies).await.0 {
         return to_login().await;
     }
     let forms = FormsTemplate { id: "12e4" }; // instantiate your struct
     forms.into_response()
 }
 
+pub async fn admin(cookies: Cookies) -> Response<Body> {
+    if !verify_cookie(&cookies).await.1{
+        return to_home().await;
+    }
+    let forms = AdminTemplate { name: "Hello" }; // instantiate your struct
+    forms.into_response()
+}
 
 
 pub async fn to_login() -> Response<Body> {
     Redirect::to("/login").into_response()
+}
+
+pub async fn to_home() -> Response<Body> {
+    Redirect::to("/").into_response()
 }
