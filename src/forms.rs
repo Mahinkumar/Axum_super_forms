62a92dd0f25e5b75a1b15771a73a62499352cadc
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-09-09 15:25:56 +0530

    Setup Selective Routing for forms

--- src/forms.rs
+++ src/forms.rs
@@ -2,7 +2,7 @@
 use askama::Template;
 use askama_axum::IntoResponse;
 use crate::{db::get_form_fields, jwt_auth::verify_cookie, router::to_login, DbPools};
-use axum::{body::Body, extract::State, http::Response, routing::get, Router};
+use axum::{body::Body, extract::{Path, State}, http::Response, routing::get, Router};
 use tower_cookies::{CookieManagerLayer, Cookies};
 
 pub struct FormField{
@@ -22,13 +22,12 @@ pub struct FormsTemplate<'a> {
 
 pub fn form_router() -> Router<DbPools> {
     Router::new()
-        .route("/forms", get(forms))
+        .route("/forms/:id", get(forms))
         .layer(CookieManagerLayer::new())
     //  .layer(TraceLayer::new_for_http()) // For Debug only
 }
 
-pub async fn forms(State(db_pools): State<DbPools>,cookies: Cookies) -> Response<Body> {
-    let form_id = "0d00".to_string();
+pub async fn forms(State(db_pools): State<DbPools>,cookies: Cookies, Path(form_id): Path<String>) -> Response<Body> {
     let cookie_ver = verify_cookie(&cookies, "Access_token_user".to_string()).await;
     if !cookie_ver.is_user {
         return to_login().await;
