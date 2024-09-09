5b2e972c9e1997a6dd3510d1c2cd2cf6ab480c51
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-09-09 22:37:44 +0530

    Rebuild data initialization Queries into single transaction

--- src/db.rs
+++ src/db.rs
@@ -7,7 +7,6 @@ use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, Postgres};
 use std::env;
 
-
 //userid,email,username,passkey
 #[derive(Debug, Deserialize, FromRedisValue, Serialize, ToRedisArgs)]
 pub struct User {
@@ -28,8 +27,11 @@ pub async fn get_db_conn_pool() -> sqlx::Pool<Postgres> {
 
 pub async fn setup_db(conn: &sqlx::Pool<Postgres>) {
     sqlx::migrate!("./migrations")
-    .run(conn)
-    .await.expect("Unable to perform migrations");
+        .run(conn)
+        .await
+        .expect("Unable to perform migrations");
+
+    let mut transaction = conn.begin().await.expect("Unable to get transaction lock");
 
     let admin_email =
         &env::var("DEFAULT_ADMIN_MAIL").expect("env variable DEFAULT_ADMIN_MAIL must be set!");
@@ -47,31 +49,36 @@ pub async fn setup_db(conn: &sqlx::Pool<Postgres>) {
     sqlx::query("INSERT INTO admins(aid,email,username,passhash) VALUES(0,$1,'ADMIN',$2) ON CONFLICT DO NOTHING;")
         .bind(admin_email)
         .bind(admin_hash)
-        .execute(conn)
+        .execute(&mut *transaction)
         .await
         .expect("Unable to create DEFAULT ADMIN in admins table");
 
     sqlx::query("INSERT INTO forms_user(userid,email,username,passkey) VALUES(0,$1,'ADMIN',$2) ON CONFLICT DO NOTHING;")
         .bind(admin_email)
         .bind(admin_passkey)
-        .execute(conn)
+        .execute(&mut *transaction)
         .await
         .expect("Unable to create DEFAULT ADMIN in forms_user table");
 
     sqlx::query("INSERT INTO user_group(uqid,userid,gid) VALUES(0,0,1) ON CONFLICT DO NOTHING;")
-        .execute(conn)
+        .execute(&mut *transaction)
         .await
         .expect("Unable to create DEFAULT user_group in forms_user table");
 
     sqlx::query("INSERT INTO form_register(fid,gid,form_name) VALUES('0d00',0,'Test_Form') ON CONFLICT DO NOTHING;")
-        .execute(conn)
+        .execute(&mut *transaction)
         .await
         .expect("Unable to create DEFAULT form in forms table");
 
     sqlx::query("INSERT INTO forms(elid,fid,typ,req,field_name,question) VALUES(0,'0d00','text',true,'name','What is your name?'),(1,'0d00','email',true,'email','What is your Email?') ON CONFLICT DO NOTHING;")
-        .execute(conn)
+        .execute(&mut *transaction)
         .await
         .expect("Unable to create DEFAULT form in forms table");
+
+    transaction
+        .commit()
+        .await
+        .expect("Unable to complete setup transactions");
 }
 
 pub async fn ping_db(conn: &sqlx::Pool<Postgres>) -> bool {
