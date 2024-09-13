a29058129b61e02cdfb05635e6aef769ae686965
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-09-13 10:39:21 +0530

    Move CLI and Server Mangement to Server module

--- src/server.rs
+++ src/server.rs
@@ -1,1 +1,32 @@
 // Server maintenance and check code here.
+
+use crate::{db::{get_db_conn_pool, ping_db, redis_load, setup_db}, mem_kv::{get_redis_pool, ping}};
+
+async fn check_network(){
+    let (redis_pool,postgres_pool) = (get_redis_pool().await,get_db_conn_pool().await);
+    println!(
+        "Redis Server Status          : {}",
+        if ping(&redis_pool).await {
+            "Active"
+        } else {
+            "Unable to connect"
+        }
+    );
+    println!(
+        "Postgres Server Status       : {}",
+        if ping_db(&postgres_pool).await {
+            "Active"
+        } else {
+            "Unable to connect"
+        }
+    );
+}
+
+pub async fn initialize(){
+    let (redis_pool,postgres_pool) = (get_redis_pool().await,get_db_conn_pool().await);
+    println!("=================================================================");
+    println!("Starting Axum Super forms Server.");
+    check_network().await;
+    setup_db(&postgres_pool).await;
+    redis_load(&postgres_pool, &redis_pool).await;
+}
